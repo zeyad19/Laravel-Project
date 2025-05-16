18fa7c981049df0fac31f869b2fcd17f12663445
Abd Al Rahman Aziz <111775063+Abd-El-RahmanAziz4869@users.noreply.github.com>
2025-05-16 20:57:23 +0300

    auth

---
+++ my-jop-board/src/stores/auth.js
@@ -0,0 +1,30 @@
+import { defineStore } from 'pinia'
+import axios from 'axios'
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    token: localStorage.getItem('token') || null,
+    user: JSON.parse(localStorage.getItem('user') || 'null')
+  }),
+  getters: {
+    isAuthenticated: (state) => !!state.token
+  },
+  actions: {
+    setToken(token) {
+      this.token = token
+      localStorage.setItem('token', token)
+      axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+    },
+    setUser(user) {
+      this.user = user
+      localStorage.setItem('user', JSON.stringify(user))
+    },
+    logout() {
+      this.token = null
+      this.user = null
+      localStorage.removeItem('token')
+      localStorage.removeItem('user')
+      delete axios.defaults.headers.common['Authorization']
+    }
+  }
+})
